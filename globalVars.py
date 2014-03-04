c05169854b8a5e4446d09c9a077d1aaea317251e
Elena Caraba <suguleasa@gmail.com>
2014-03-04 17:09:17 -0600

    setting file

---
+++ globalVars.py
@@ -0,0 +1,13 @@
+binBnd = [0,110,256]
+AREA_INCLUSION = 16.0 # or a 4x4 small inclusion of pixels
+PROB = 0.01
+MAX_SIZE = 20
+MIN_SIZE = 16#6
+ALT_MIN_SIZE = 32#3
+TOL_LINEARS = 2.0
+TOL_QUADRATICS = -2.0 # cancel the cubic approximation by making a negative number
+TOL_CUBICS = -2.0 # cancel the cubic approximation by making a negative number
+
+TOL_error = 3
+
+VAL = 1
