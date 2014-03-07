799af6f0f7954278c646d7d6880a250e7b05fc22
Elena Caraba <suguleasa@gmail.com>
2014-03-07 17:13:51 -0600

    generalized data creation

--- MatlabPrograms/create3Ddataset.m
+++ MatlabPrograms/create3Ddataset.m
@@ -1,36 +1,39 @@
-len1 = 500;
-len2 = 200;
-len3 = 500;
+clear all;
+display 'creating data set'
+base = 128;
+len1 = 4*base;
+len2 = 2*base;
+len3 = 4*base;
 
 mat = 105*ones(len1,len2,len3);
 mat = uint8(mat);
 
-%zf = @(x) 2 * ( cos(x/8) + 10);
-zf1 = @(x) cos(x/50) + 2;
-zf2 = @(x) cos(x/50) + 2;
-
-R1 = 50;
-R2 = 100;
-
-xshift1 = 50;
-yshift1 = 50;
-
-xshift2 = 150;
-yshift2 = 150;
+radius = [0.5*base, 0.5*base];
+center = [[0.25*len1, 0.5*len2]; [0.75*len1, 0.5*len2]];
+frequency = [[1/50, 1/75]; [1/50, 1/75]];
+amplitude = [[0.05*len1, 0*len2]; [0.05*len1, 0*len2]];
+phase = [[1, 2]; [1, 2]];
+numChannels = length(radius);
 
 for i=1:len1
 	for j=1:len2
-		for k = 1:len3
-			if ( i - xshift1 * zf1(k) ) ^ 2 + (j - yshift1) ^ 2 <= R1 * R1
-				mat(i,j,k) = 220;
-			end
-			if ( i - xshift2 * zf2(k) ) ^ 2 + (j - yshift2) ^ 2 <= R2 * R2
-				mat(i,j,k) = 220;
+		for k=1:len3
+			for c=1:numChannels
+				if (i - center(c,1) - amplitude(c,1)*(cos(k*frequency(c,1)+phase(c,1))))^2 + ...
+					 (j - center(c,2) - amplitude(c,2)*(cos(k*frequency(c,2)+phase(c,2))))^2 <= radius(c)^2
+					mat(i,j,k) = 220;
+				end
 			end
 		end
 	end
 end
 
-imshow(squeeze(mat(:,35,:)))
-imshow(squeeze(mat(:,:,30)))
+disp 'done, writing out'
+figure;
+imshow(squeeze(mat(:,100,:)))
+figure;
+imshow(squeeze(mat(:,:,250)))
 
+dicomwrite(reshape(mat,len1,len2,1,len3),'channels_512x256.dcm')
+
+%gipl_write_volume(mat)
