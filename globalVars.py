20c9bcddb14d7dc9b201af291c0bdbe554c16cbd
Elena Caraba <suguleasa@gmail.com>
2014-10-19 16:49:37 -0500

    cleaned up and added comments

--- globalVars.py
+++ globalVars.py
@@ -1,36 +1,33 @@
-#binBnd = [0,200,256] # for real slice: microvascular
-#binBnd = [0,110,256] # for fake images
-#binBnd = [0,140,256] # for fake doughnut images
-#binBnd = [0,90,256] # for Green Circles image: green1.png
-#binBnd = [0,100,256] # for SiBat.png
-binBnd = [0,150,256] # for SiBat_filtered.png
-#binBnd = [0,170,256] # for SnBat.png
-#binBnd = [0,200,256] # for SnBat_0054.png
-#binBnd = [0,50,256] # for doughnut image
-#binBnd = [0,110,256]
-#binBnd = [0,200,256] # for SnBat
-AREA_INCLUSION = 1048.0 # or a 4x4 small inclusion of pixels
-PROB = 0.3
-#MAX_SIZE = 80
-#MIN_SIZE = 20#6
-ALT_MIN_SIZE = 40#3
-TOL_LINEARS = 2.0
-TOL_QUADRATICS = -2.0 # cancel the cubic approximation by making a negative number
-TOL_CUBICS = -2.0 # cancel the cubic approximation by making a negative number
+# Developer: Elena Caraba, copyright 2014
 
-TOL_error = 3
+# File setting up the experiment
+
+
+# setting the boundaries for the pixel colors bins
+binBnd = [0,150,256] # for SiBat_filtered.png
+
+
+VOL_INCLUSION = 1048.0 # volume of significant inclusion
+PROB = 0.3 # probability an inclusion of vol_inclusion size was missed
+
+
+# steps size for plotting / drawing NURBS in element
 T_STEP = 0.001
 
-VAL = 0
+VAL = 0 # pixel color for drawing
 
+# set element max and min size
 MAX_SIZE_X = 127
-MIN_SIZE_X = 50
+MIN_SIZE_X = 127
 
 MAX_SIZE_Y = 127
-MIN_SIZE_Y = 50
+MIN_SIZE_Y = 127
 
 MAX_SIZE_Z = 127
-MIN_SIZE_Z = 50
+MIN_SIZE_Z = 127
+
+ALT_MIN_SIZE = 40 # alternative minimum size of elements
+
 
 # K-neighbor rule
 k1_CONST = 5
@@ -40,8 +37,10 @@ k2_CONST = 2
 N_ELEMS = 4
 STRESS_MIN = 4
 
+# allow NURBS approximation of interface
 NURBS_ON = 1
-
+# pixel tolerance for NURBS approximation
 TOL_NURBS = 3.0
 
+# number of interior points: GRID_PTS x GRID_PTS for Coons NURBS surfaces
 GRID_PTS = 2 # grid of 2 x 2
